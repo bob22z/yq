a4abb67727af317f0cbdd3518bffd2aa1521e70f
Bob <bob22.zhang@gmail.com>
2023-03-31 14:31:23 -0700

    yq-scheduler: get redis_url from ENV or ARG

--- yq-scheduler/src/main.rs
+++ yq-scheduler/src/main.rs
@@ -2,14 +2,30 @@ use yq_scheduler::Scheduler;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let subscriber = tracing_subscriber::FmtSubscriber::builder()
-        .with_max_level(tracing::Level::INFO)
-        .with_env_filter("yq=TRACE,yq_scheduler=TRACE")
-        .finish();
-    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+    init_tracing();
 
-    let scheduler = Scheduler::new("redis://127.0.0.1/").await?;
+    let redis_url = try_get_redis_url()?;
+    let scheduler = Scheduler::new(&redis_url).await?;
     scheduler.run().await?;
 
     Ok(())
+}
+
+fn try_get_redis_url() -> Result<String, Box<dyn std::error::Error>> {
+    if let Ok(redis_url) = std::env::var("YQ_REDIS_URL") {
+        return Ok(redis_url);
+    }
+
+    let mut args = std::env::args();
+    args.next();
+
+    args.next()
+        .ok_or_else(|| "Could not get YQ_REDIS_URL from ENV or ARGS".into())
+}
+
+fn init_tracing() {
+    let subscriber = tracing_subscriber::FmtSubscriber::builder()
+        .with_max_level(tracing::Level::INFO)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 }
